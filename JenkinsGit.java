04238957d4c5f062abea0028f0f4ac9e9237e920
97-Siva <95157560+97-Siva@users.noreply.github.com>
2021-12-10 06:58:33 +0530

    initial commit

---
+++ JenkinsGit.java
@@ -0,0 +1,8 @@
+public class JenkinsGit{
+	public static void main(String[] args){
+		System.out.println("This is the first");
+		System.out.println("This is the second");
+		System.out.println("This is the third");
+		System.out.println("This is the fourth");
+	}
+}
